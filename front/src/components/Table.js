ff3a3392192afe183b0a1715d06cbe3a95f7f1ab
hyeonah <hyeonah.hello@gmail.com>
2021-12-31 22:28:52 +0900

    create pagination on table

--- front/src/components/Table.js
+++ front/src/components/Table.js
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import './Table.scss';
-import { useTable } from 'react-table';
+import { useTable, usePagination } from 'react-table';
 import Saramin from '../saramin.json';
 import { Columns } from './Columns';
 
@@ -8,38 +8,72 @@ const Table = () => {
   const columns = useMemo(() => Columns, []);
   const data = useMemo(() => Saramin, []);
 
-  const tableInstance = useTable({
-    columns: columns,
-    data: data,
-  });
+  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+  //   useTable(
+  //     {
+  //       columns: columns,
+  //       data: data,
+  //     },
+  //     usePagination
+  //   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  const tableInstance = useTable(
+    {
+      columns: columns,
+      data: data,
+    },
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+  } = tableInstance;
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <div>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
