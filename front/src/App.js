7b404b900dc368c05694ab971cb9eca65bb3035a
hyeonah <hyeonah.hello@gmail.com>
2021-12-31 19:22:50 +0900

    Revert "create layout and filter section"
    This reverts commit 8f8bcb558193f30ed561918e2be6dd23058cc90d.

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,23 @@
-import Dashboard from './components/Dashboard';
-import Filter from './components/Filter';
-import Search from './components/Search';
-import Result from './components/Result';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <Dashboard>
-        <Filter></Filter>
-        <Search></Search>
-        <Result></Result>
-      </Dashboard>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
