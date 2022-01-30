8f49595ecce6a888ec66337c3930098f9b66f3e1
hyeonah <hyeonah.hello@gmail.com>
2022-01-30 15:09:35 +0900

    Update ajax code, content typo, add search_human.json

--- front/src/context/job-post-context.js
+++ front/src/context/job-post-context.js
@@ -1,8 +1,9 @@
 import { useState, createContext, useEffect } from 'react';
 import axios from 'axios';
 
-const Mock_API = '/MOCK_DATA.json';
-// const Mock_API = process.env.REACT_APP_SERVER_HOST;
+const saraminData = '/MOCK_DATA.json';
+// const saraminData = '/search_human.json';
+// const saraminData = process.env.REACT_APP_SERVER_HOST;
 // console.log(process.env);
 
 const JobPostContext = createContext({
@@ -18,16 +19,24 @@ const JobPostProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(Mock_API);
-        // console.log(res.data.contents);
-        setPosts(res.data.contents);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchData();
+    // const fetchData = async () => {
+    //   try {
+    //     const res = await axios.get(saraminData);
+    //     // console.log(res.data.content);
+    //     setPosts(res.data.content);
+    //   } catch (e) {
+    //     console.error(e);
+    //   }
+    // };
+    // fetchData(saraminData);
+    axios
+      .get(saraminData)
+      .then(({ data: { content } }) => {
+        setPosts(content);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }, []);
 
   const value = {
